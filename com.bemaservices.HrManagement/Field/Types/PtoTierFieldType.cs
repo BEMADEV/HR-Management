4bea2fa3b224901ad23165ec4484cf3cddfcbfab
Taylor Cavaletto <tjcavaletto@gmail.com>
2025-08-01 09:20:51 -0400

    - Switched HR Management to the universal field types

--- com.bemaservices.HrManagement/Field/Types/PtoTierFieldType.cs
+++ com.bemaservices.HrManagement/Field/Types/PtoTierFieldType.cs
@@ -17,20 +17,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 using com.bemaservices.HrManagement.Model;
-using com.bemaservices.HrManagement.Web.UI.Controls;
-using com.bemaservices.HrManagement.Web.UI.Controls.Pickers;
 using Rock;
 using Rock.Data;
 using Rock.Field;
-using Rock.MergeTemplates;
-using Rock.Reporting;
+using Rock.Field.Types;
+using Rock.ViewModels.Utility;
 using Rock.Web.Cache;
-using Rock.Web.UI.Controls;
 
 namespace com.bemaservices.HrManagement.Field.Types
 {
@@ -41,279 +35,44 @@ namespace com.bemaservices.HrManagement.Field.Types
     /// </summary>
     /// <seealso cref="FieldType" />
     /// <seealso cref="IEntityFieldType" />
-    class PtoTierFieldType : Rock.Field.FieldType, IEntityFieldType
+    class PtoTierFieldType : UniversalItemPickerFieldType, IEntityFieldType
     {
-
-        #region Formatting
-
         /// <summary>
-        /// Returns the field's current value(s)
+        /// Gets the list of items to be displayed in the picker.
         /// </summary>
-        /// <param name="parentControl">The parent control.</param>
-        /// <param name="value">Information about the value</param>
-        /// <param name="configurationValues">The configuration values.</param>
-        /// <param name="condensed">Flag indicating if the value should be condensed (i.e. for use in a grid column)</param>
-        /// <returns>System.String.</returns>
-        public override string FormatValue( System.Web.UI.Control parentControl, string value, Dictionary<string, ConfigurationValue> configurationValues, bool condensed )
+        /// <param name="privateConfigurationValues">The configuration values that describe the field type.</param>
+        /// <returns>A list of item bags that will be rendered in the picker.</returns>
+        protected override List<ListItemBag> GetListItems( Dictionary<string, string> privateConfigurationValues )
         {
-            string formattedValue = value;
 
-            if ( !string.IsNullOrWhiteSpace( value ) )
+            return new PtoTierService( new RockContext() ).Queryable()
+            .Select( item => new ListItemBag
             {
-                var ptoTier = new PtoTierService( new RockContext() ).Get( value.AsGuid() );
-                if ( ptoTier != null )
-                {
-                    formattedValue = ptoTier.Name;
-                }
-            }
-
-            return base.FormatValue( parentControl, formattedValue, configurationValues, condensed );
+                Value = item.Guid.ToString(),
+                Text = item.Name
+            } )
+            .ToList();
         }
 
-        #endregion
-
-        #region Edit Control
-
         /// <summary>
-        /// Creates the control(s) necessary for prompting user for a new value
+        /// Gets the item bags for the values. If an item is not found
+        /// (for example, no longer exists), then it should not be included
+        /// in the returned list.
         /// </summary>
-        /// <param name="configurationValues">The configuration values.</param>
-        /// <param name="id">The id.</param>
-        /// <returns>The control</returns>
-        public override System.Web.UI.Control EditControl( Dictionary<string, ConfigurationValue> configurationValues, string id )
+        /// <param name="values">The individual values that should be retrieved.</param>
+        /// <param name="privateConfigurationValues">The private (database) configuration values.</param>
+        /// <returns>A list of <see cref="T:Rock.ViewModels.Utility.ListItemBag" /> objects that have the <see cref="P:Rock.ViewModels.Utility.ListItemBag.Value" /> and <see cref="P:Rock.ViewModels.Utility.ListItemBag.Text" /> properties filled in.</returns>
+        protected override List<ListItemBag> GetItemBags( IEnumerable<string> values, Dictionary<string, string> privateConfigurationValues )
         {
-            var ptoTierPicker = new PtoTierPicker { ID = id };
-
-            var allPtoTiers = new PtoTierService( new RockContext() ).Queryable();
-
-            var ptoTiersList = allPtoTiers
-                .ToList();
-
-            if ( allPtoTiers.Any() )
+            return new PtoTierService( new RockContext() ).Queryable()
+            .Where( item => values.Contains( item.Guid.ToString() ) )
+            .Select( item => new ListItemBag
             {
-                ptoTierPicker.PtoTiers = ptoTiersList;
-                return ptoTierPicker;
-            }
-
-            return null;
+                Value = item.Guid.ToString(),
+                Text = item.Name
+            } )
+            .ToList();
         }
-
-        /// <summary>
-        /// Reads new values entered by the user for the field
-        /// returns PtoType Entity Type Guid as string
-        /// </summary>
-        /// <param name="control">Parent control that controls were added to in the CreateEditControl() method</param>
-        /// <param name="configurationValues">The configuration values.</param>
-        /// <returns>System.String.</returns>
-        public override string GetEditValue( System.Web.UI.Control control, Dictionary<string, ConfigurationValue> configurationValues )
-        {
-            PtoTierPicker ptoTierPicker = control as PtoTierPicker;
-
-            if ( ptoTierPicker != null )
-            {
-                int? ptoTierId = ptoTierPicker.SelectedPtoTierId;
-                if ( ptoTierId.HasValue )
-                {
-                    var ptoTier = new PtoTierService( new RockContext() ).Get( ptoTierId.Value );
-                    if ( ptoTier != null )
-                    {
-                        return ptoTier.Guid.ToString();
-                    }
-                }
-            }
-
-            return string.Empty;
-        }
-
-        /// <summary>
-        /// Sets the value.
-        /// Expects value as a ReportTemplate Entity Type Guid as string
-        /// </summary>
-        /// <param name="control">The control.</param>
-        /// <param name="configurationValues">The configuration values.</param>
-        /// <param name="value">The value.</param>
-        public override void SetEditValue( System.Web.UI.Control control, Dictionary<string, ConfigurationValue> configurationValues, string value )
-        {
-            PtoTierPicker ptoTierPicker = control as PtoTierPicker;
-
-            if ( ptoTierPicker != null )
-            {
-                Guid guid = value.AsGuid();
-
-                // get the item (or null) and set it
-                var ptoTier = new PtoTierService( new RockContext() ).Get( guid );
-                ptoTierPicker.SetValue( ptoTier == null ? "0" : ptoTier.Id.ToString() );
-            }
-        }
-
-        #endregion
-
-        #region Filter Control
-
-        /// <summary>
-        /// Gets the filter compare control.
-        /// </summary>
-        /// <param name="configurationValues">The configuration values.</param>
-        /// <param name="id">The identifier.</param>
-        /// <param name="required">if set to <c>true</c> [required].</param>
-        /// <param name="filterMode">The filter mode.</param>
-        /// <returns>Control.</returns>
-        public override Control FilterCompareControl( Dictionary<string, ConfigurationValue> configurationValues, string id, bool required, FilterMode filterMode )
-        {
-            var lbl = new Label();
-            lbl.ID = string.Format( "{0}_lIs", id );
-            lbl.AddCssClass( "data-view-filter-label" );
-            lbl.Text = "Is";
-
-            // hide the compare control when in SimpleFilter mode
-            lbl.Visible = filterMode != FilterMode.SimpleFilter;
-
-            return lbl;
-        }
-
-        /// <summary>
-        /// Gets the filter value control.
-        /// </summary>
-        /// <param name="configurationValues">The configuration values.</param>
-        /// <param name="id">The identifier.</param>
-        /// <param name="required">if set to <c>true</c> [required].</param>
-        /// <param name="filterMode">The filter mode.</param>
-        /// <returns>Control.</returns>
-        public override Control FilterValueControl( Dictionary<string, ConfigurationValue> configurationValues, string id, bool required, FilterMode filterMode )
-        {
-            var cbList = new RockCheckBoxList();
-            cbList.ID = string.Format( "{0}_cbList", id );
-            cbList.AddCssClass( "js-filter-control" );
-            cbList.RepeatDirection = RepeatDirection.Horizontal;
-
-            var ptoTierList = new PtoTierService( new RockContext() ).Queryable();
-            if ( ptoTierList.Any() )
-            {
-                foreach ( var ptoTier in ptoTierList )
-                {
-                    ListItem listItem = new ListItem( ptoTier.Name, ptoTier.Guid.ToString() );
-                    cbList.Items.Add( listItem );
-                }
-
-                return cbList;
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        /// Formats the filter value value.
-        /// </summary>
-        /// <param name="configurationValues">The configuration values.</param>
-        /// <param name="value">The value.</param>
-        /// <returns>System.String.</returns>
-        public override string FormatFilterValueValue( Dictionary<string, ConfigurationValue> configurationValues, string value )
-        {
-            var ptoTierGuids = value.SplitDelimitedValues().AsGuidList();
-            var ptoTierService = new PtoTierService( new RockContext() );
-
-            var ptoTiers = ptoTierGuids.Select( a => ptoTierService.Get( a ) ).Where( c => c != null );
-            return ptoTiers.Select( a => a.Name ).ToList().AsDelimited( ", ", " or " );
-        }
-
-        /// <summary>
-        /// Gets the filter compare value.
-        /// </summary>
-        /// <param name="control">The control.</param>
-        /// <param name="filterMode">The filter mode.</param>
-        /// <returns>System.String.</returns>
-        public override string GetFilterCompareValue( Control control, FilterMode filterMode )
-        {
-            return null;
-        }
-
-        /// <summary>
-        /// Gets the equal to compare value (types that don't support an equalto comparison (i.e. singleselect) should return null
-        /// </summary>
-        /// <returns>System.String.</returns>
-        public override string GetEqualToCompareValue()
-        {
-            return null;
-        }
-
-        /// <summary>
-        /// Gets the filter value value.
-        /// </summary>
-        /// <param name="control">The control.</param>
-        /// <param name="configurationValues">The configuration values.</param>
-        /// <returns>System.String.</returns>
-        public override string GetFilterValueValue( Control control, Dictionary<string, ConfigurationValue> configurationValues )
-        {
-            var values = new List<string>();
-
-            if ( control != null && control is CheckBoxList )
-            {
-                CheckBoxList cbl = ( CheckBoxList ) control;
-                foreach ( ListItem li in cbl.Items )
-                {
-                    if ( li.Selected )
-                    {
-                        values.Add( li.Value );
-                    }
-                }
-            }
-
-            return values.AsDelimited( "," );
-        }
-
-        /// <summary>
-        /// Sets the filter compare value.
-        /// </summary>
-        /// <param name="control">The control.</param>
-        /// <param name="value">The value.</param>
-        public override void SetFilterCompareValue( Control control, string value )
-        {
-        }
-
-        /// <summary>
-        /// Sets the filter value value.
-        /// </summary>
-        /// <param name="control">The control.</param>
-        /// <param name="configurationValues">The configuration values.</param>
-        /// <param name="value">The value.</param>
-        public override void SetFilterValueValue( Control control, Dictionary<string, ConfigurationValue> configurationValues, string value )
-        {
-            if ( control != null && control is CheckBoxList && value != null )
-            {
-                var values = value.Split( new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries ).ToList();
-
-                CheckBoxList cbl = ( CheckBoxList ) control;
-                foreach ( ListItem li in cbl.Items )
-                {
-                    li.Selected = values.Contains( li.Value );
-                }
-            }
-        }
-
-        /// <summary>
-        /// Gets the filters expression.
-        /// </summary>
-        /// <param name="configurationValues">The configuration values.</param>
-        /// <param name="filterValues">The filter values.</param>
-        /// <param name="parameterExpression">The parameter expression.</param>
-        /// <returns>Expression.</returns>
-        public override Expression AttributeFilterExpression( Dictionary<string, ConfigurationValue> configurationValues, List<string> filterValues, ParameterExpression parameterExpression )
-        {
-            if ( filterValues.Count == 1 )
-            {
-                List<string> selectedValues = filterValues[0].Split( new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries ).ToList();
-                if ( selectedValues.Any() )
-                {
-                    MemberExpression propertyExpression = Expression.Property( parameterExpression, "Value" );
-                    ConstantExpression constantExpression = Expression.Constant( selectedValues, typeof( List<string> ) );
-                    return Expression.Call( constantExpression, typeof( List<string> ).GetMethod( "Contains", new Type[] { typeof( string ) } ), propertyExpression );
-                }
-            }
-
-            return null;
-        }
-
-        #endregion
 
         #region Entity Methods
 
